ae12d127a6135181c7fdc6e50a4c1a1cc4724d4f
Manoj <manojgodhani00@gmail.com>
2017-08-25 00:59:31 -0700

    change video clip

--- src/components/problems.js
+++ src/components/problems.js
@@ -2,7 +2,7 @@
 
 export const problems = [
   {
-    video: "https://s3-ap-southeast-1.amazonaws.com/aqs-minapps/media/videos/20170818_120143_edited.mp4",
+    video: "https://s3-ap-southeast-1.amazonaws.com/aqs-minapps/media/videos/20170825_134134_edited.mp4",
   },
   {
     video: "https://s3-ap-southeast-1.amazonaws.com/aqs-minapps/media/videos/20170818_120210_edited.mp4",
