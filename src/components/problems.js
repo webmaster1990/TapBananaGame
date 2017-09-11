70943c2e38f631889663f24ce3146eccd1809fa7
manoj Godhani <mayurgodhani00@gmail.com>
2017-09-10 23:27:55 -0700

    update video

--- src/components/problems.js
+++ src/components/problems.js
@@ -4,7 +4,10 @@ export const problems = [
   {
     video: "https://s3-ap-southeast-1.amazonaws.com/aqs-minapps/media/videos/20170825_134134_edited.mp4",
   },
+//   {
+//     video: "https://s3-ap-southeast-1.amazonaws.com/aqs-minapps/media/videos/20170818_120210_edited.mp4",
+//   }
   {
-    video: "https://s3-ap-southeast-1.amazonaws.com/aqs-minapps/media/videos/20170818_120210_edited.mp4",
+    video:"https://s3-ap-southeast-1.amazonaws.com/aqs-minapps/media/videos/20170818_120143_edited.mp4",
   }
 ]
